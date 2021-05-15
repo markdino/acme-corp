d9025dfe997b401d3eba9398a87c2fca46f783af
markdino <markdinopelonia447@gmail.com>
2021-05-15 11:44:16 +0800

    fix(Button): Increase lift and right padding by 5px

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const Button = styled.button`
   color: #fff;
-  padding: 10px;
+  padding: 10px 15px;
   border: 1px solid #fff;
   font-weight: bold;
   font-size: 0.8rem;
